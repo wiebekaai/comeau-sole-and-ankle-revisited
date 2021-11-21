c2bd620cd96985921b4dda02393ecc1d73e194a0
Wiebe Kaai <wiebekaai@gmail.com>
2021-11-21 13:33:18 +0100

    Add query constants

--- src/constants.js
+++ src/constants.js
@@ -16,3 +16,16 @@ export const WEIGHTS = {
   medium: 600,
   bold: 800,
 };
+
+const BREAKPOINTS = {
+  phone: 600 / 16,
+  tablet: 950 / 16,
+  laptop: 1300 / 16
+}
+
+const query = b => `(max-width: ${b}rem)`;
+
+export const QUERIES = {
+  tablet: query(BREAKPOINTS.desktop),
+  phone: query(BREAKPOINTS.laptop)
+}
